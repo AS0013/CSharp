ab5bd65a9288acf0266a38af420e170cc5f4c77e
AS0013 <86631457+AS0013@users.noreply.github.com>
2023-07-05 02:07:45 +0200

    small comment adjustments

--- csharp/SinglyLinkedListProblems.cs
+++ csharp/SinglyLinkedListProblems.cs
@@ -38,8 +38,8 @@ public class SinglyLinkedListProblems{
     }
 
     // Leetcode: Easy
-    //Given the head of a singly linked list, return the middle node of the linked list.
-    //If there are two middle nodes, return the second middle node.
+    // Given the head of a singly linked list, return the middle node of the linked list.
+    // If there are two middle nodes, return the second middle node.
 
     // Using Floyd’s slow and fast pointers approach. 
 
